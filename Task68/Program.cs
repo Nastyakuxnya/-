0b321b3c7f5b87926cb4543b714eadf683a078e3
Anastyasiya Kalinkina <nastyanikonova17@mail.ru>
2024-06-21 15:01:34 +0300

    Дамашня работа (промежуточная аттестация), задание 64,66,68

---
+++ Task68/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 68:
+//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunction(int m, int n)
+{
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0)
+        {
+            return AckermanFunction(m - 1, 1);
+        }
+
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+}
+
+bool CheckRange(int numberM, int numberN)
+{
+    return (numberM > 3 &&  numberN >= 11) || ( numberM >= 4 && numberN > 0);
+  
+}
+
+if (CheckRange(numberM, numberN))
+{
+    Console.WriteLine("Некорректный ввод, введите другие M и N");
+    return;
+}
+
+int ack = AckermanFunction(numberM, numberN);
+Console.WriteLine(ack);
